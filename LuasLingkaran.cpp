ea39f76cd6e73fe3b0c2e892530218555d7f52bc
zaknisdev <zakianisf1710@gmail.com>
2025-04-28 10:47:24 +0700

    membuat method untuk input data

--- LuasLingkaran.cpp
+++ LuasLingkaran.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class persegi_panjang{
+class persegipanjang{
     private:
     int panjang;
     int lebar;
     int luas;
 
     public:
+    void inputdata()
+    {
+        cout << "Masukkan panjang = ";
+        cin >> panjang;
+        cout << "Masukkan lebar = ";
+        cin >> lebar;
+    }
 
+    
 };
 
 int main(){
-    
+
 }
