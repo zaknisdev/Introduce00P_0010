35118e1e5c604115f751479743336fcf66fb2f02
zaknisdev <zakianisf1710@gmail.com>
2025-04-28 10:39:22 +0700

    library

--- LuasLingkaran.cpp
+++ LuasLingkaran.cpp
@@ -1,9 +1,4 @@
-zaki.inputdata();
-    cout << "Nama Mobil = " << zaki.nama_mobil << endl;
-    cout << "Tipe Mobil = " << zaki.tipe_mobil << endl;
-    zaki.output_data();
+#include <iostream>
+using namespace std;
 
-    void output_data()
-    {
-        cout << "Harga Mobil = " << harga_mobil << endl;
-    }
+
